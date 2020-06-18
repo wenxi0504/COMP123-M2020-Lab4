1b2ad987f5ff0bb6c9a6121d602914d3f0448609
Wen Xu <wxu63@my.centennialcollege.ca>
2020-06-17 20:48:47 -0400

    add Person class

---
+++ COMP123-M2020-Lab4/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP123_M2020_Lab4
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+
+        public void SaysHello()
+        {
+            Console.WriteLine($"{Name}says hello!");
+
+        }
+
+
+
+
+
+    }
+}
