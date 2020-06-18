fe851b0ddb139199bc9fe36db016f84708d29cba
Wen Xu <wxu63@my.centennialcollege.ca>
2020-06-17 20:45:05 -0400

    initialise Project

---
+++ COMP123-M2020-Lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP123_M2020_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
